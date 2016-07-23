d4b834f062cf56720533e4be0cd38b0568d27c5e
HailsTails <HelenCodeMonkey@gmail.com>
2016-07-24 09:58:50 +1000

    Attempted to fix error in Child registering submission page. Cannot find the problem. Other minor changes.

--- app/src/main/java/au/com/shehacks/seven/seven/RegisterChildActivity.java
+++ app/src/main/java/au/com/shehacks/seven/seven/RegisterChildActivity.java
@@ -3,19 +3,35 @@ package au.com.shehacks.seven.seven;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
 
 /**
  * Created by hails on 23/07/16.
  */
 public class RegisterChildActivity extends BaseActivity {
+    Button createButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         inflateLayoutInContainer(R.layout.activity_register_child);
+        setViews();
     }
 
-    public void createUser(View view){
-        Intent intent = new Intent(this, CharitySelectionActivity.class);
+    private View.OnClickListener listen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            onCreateChildPressed(v);
+        }
+    };
+
+    private void setViews() {
+        createButton = (Button) findViewById(R.id.createChild);
+        createButton.setOnClickListener(listen);
+    }
+
+    public void onCreateChildPressed(View view){
+        Intent intent = new Intent(view.getContext(), CharitySelectionActivity.class);
         startActivity(intent);
     }
 }
