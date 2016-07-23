fe199609d170b8725bac2a1a38c421ab776a9874
hails <DanielJackson22@>
2016-07-24 09:58:50 +1000

    Attempted to fix error in Child registering submission page. Cannot find the problem. Other minor changes.

--- app/src/main/java/au/com/shehacks/seven/seven/DonationActivity.java
+++ app/src/main/java/au/com/shehacks/seven/seven/DonationActivity.java
@@ -1,7 +1,20 @@
 package au.com.shehacks.seven.seven;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 /**
  * Created by hails on 23/07/16.
  */
-public class DonationActivity {
+public class DonationActivity extends BaseActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        inflateLayoutInContainer(R.layout.activity_charity_selection);
+    }
+
+    public void selectCharity(View view){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+    }
 }
