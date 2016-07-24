f091bb25338ae75375a885a1eeb20f639c05f980
HailsTails <HelenCodeMonkey@gmail.com>
2016-07-24 12:53:39 +1000

    Charity Selection outline added

--- app/src/main/java/au/com/shehacks/seven/seven/CharitySelectionActivity.java
+++ app/src/main/java/au/com/shehacks/seven/seven/CharitySelectionActivity.java
@@ -1,5 +1,6 @@
 package au.com.shehacks.seven.seven;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,13 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CharitySelectionActivity extends AppCompatActivity {
+public class CharitySelectionActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_charity_selection);
+        inflateLayoutInContainer(R.layout.activity_charity_selection);
+    }
 
+    public void popUp(){
+        findViewById(R.id.popUpImage).setVisible(true);
     }
 
 }
